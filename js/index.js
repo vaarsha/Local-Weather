6b97e80d9e5217517bcbe01409e0bf0beecc1e1b
vaarsha <varshar186@gmail.com>
2017-01-06 01:32:14 +0530

    geolocation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-//javascript file
-     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        $("#loc").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+//to get users position
+var latitude, longitude;
+$(document).ready(function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      $("#loc").html("latitude: " + latitude + "<br>longitude: " + longitude);
+      $.getJSON("api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=", function(result) {
+     //update parameters
+        
+     
       });
-    }
+    });
+  } else {
+    $("#loc").html("Geolocation is not supported by this browser.");
+  }
+});
