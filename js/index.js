3c3820b82af5fdb9f3c36dc08fb2549a449628d6
vaarsha <varshar186@gmail.com>
2017-01-05 23:38:08 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 //javascript file
+     if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $("#loc").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+      });
+    }
