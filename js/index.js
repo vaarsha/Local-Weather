76b6612cf7e34a884015b7591e01a8ba9d6f7c2e
vaarsha <varshar186@gmail.com>
2017-01-07 01:20:13 +0530

    get temperature by coordinates

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ $(document).ready(function() {
       longitude = position.coords.longitude;
       $("#loc").html("latitude: " + latitude + "<br>longitude: " + longitude);
       $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=", function(result) {
-     //update parameters
-        
-     
+        //update parameters
+        console.log("hello");
+        console.log("rs :", result);
+        tmpC = Math.floor(result.main.temp);
+        $("#tmp").html(tmpC + "&#8451");
+        $("#plc").val(result.name + "," + result.sys.country);
       });
     });
   } else {
