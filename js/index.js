8e9dc54b3d8a7236acff123b85cffdc3a4e93bfa
vaarsha <varshar186@gmail.com>
2017-01-08 00:23:17 +0530

    on check conversion

--- js/index.js
+++ js/index.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
         tmpC = Math.floor(result.main.temp);
         $("#tmp").html(tmpC + "&#8451");
         $("#plc").val(result.name + "," + result.sys.country);
+        $("input[type=checkbox]").on("click",function(){
+       change(tmpC); 
+      });
       });
     });
   } else {
@@ -37,6 +40,9 @@ function handle(e) {
     $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + res + "&units=metric&APPID=c213f9ca1b6333f1b214582c52904e92", function(val) {
       tmpC = Math.floor(val.main.temp);
       $("#tmp").html(tmpC + "&#8451");
+      $("input[type=checkbox]").on("click",function(){
+       change(tmpC); 
+      });
     });
   }
 }
