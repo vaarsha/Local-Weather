69a53a13e03d06ba631d1872e76c522b39437f8d
vaarsha <varshar186@gmail.com>
2017-01-08 00:25:24 +0530

    added weather description

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
         tmpC = Math.floor(result.main.temp);
         $("#tmp").html(tmpC + "&#8451");
         $("#plc").val(result.name + "," + result.sys.country);
+        $("#cont").html(result.weather[0].description);  
         $("input[type=checkbox]").on("click",function(){
        change(tmpC); 
       });
@@ -40,6 +41,7 @@ function handle(e) {
     $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + res + "&units=metric&APPID=c213f9ca1b6333f1b214582c52904e92", function(val) {
       tmpC = Math.floor(val.main.temp);
       $("#tmp").html(tmpC + "&#8451");
+      $("#cont").html(val.weather[0].description); 
       $("input[type=checkbox]").on("click",function(){
        change(tmpC); 
       });
