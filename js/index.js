0a9fbb24fd6c6aee5eaf79dfdd9b8d9ae6a56e27
vaarsha <varshar186@gmail.com>
2017-01-08 00:30:54 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
-      $("#loc").html("latitude: " + latitude + "<br>longitude: " + longitude);
       $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=c213f9ca1b6333f1b214582c52904e92", function(result) {
         //update parameters
         console.log("hello");
